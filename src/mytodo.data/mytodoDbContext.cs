43937a063944aec63913595fbbf5221abb0587dd
BrendomGoncalves <brendom.vi@gmail.com>
2024-05-28 00:25:40 -0300

    feat: Implementação do contexto

--- src/mytodo.data/mytodoDbContext.cs
+++ src/mytodo.data/mytodoDbContext.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using mytodo.domain.Entities;
+
 namespace mytodo.data;
 
-public class mytodoDbContext
+public class MytodoDbContext : DbContext
 {
+    public DbSet<UserEntity> User { get; set; } = default!;
+    public DbSet<TaskEntity> Task { get; set; } = default!;
     
+    public MytodoDbContext(DbContextOptions<MytodoDbContext> options) : base(options){}
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MytodoDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
