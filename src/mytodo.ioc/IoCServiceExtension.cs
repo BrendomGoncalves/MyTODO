b29fab74a672bce52d5b086b42fe6a41b2c03933
BrendomGoncalves <brendom.vi@gmail.com>
2024-05-28 00:28:30 -0300

    feat: Criando classe para configurar injeções de dependencias e banco de dados

--- src/mytodo.ioc/IoCServiceExtension.cs
+++ src/mytodo.ioc/IoCServiceExtension.cs
@@ -1,6 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using mytodo.data;
+using mytodo.domain.Services;
+using mytodo.ioc.Services;
+
 namespace mytodo.ioc;
 
-public class IoCServiceExtension
+public static class IoCServiceExtension
 {
-    
+    public static void ConfigureAppDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        ConfigureDbContext(services, configuration);
+        services.AddScoped<IEncryptionService, EncryptionService>();
+    }
+
+    private static void ConfigureDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<MytodoDbContext>((sp, options) =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("MytodoDbContext"));
+        });
+
+        services.AddScoped<InitializeDatabaseApplication>();
+    }
 }
