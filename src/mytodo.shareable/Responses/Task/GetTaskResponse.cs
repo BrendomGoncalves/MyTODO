d80c2be8e0d1603f71413672b629c608919373cf
BrendomGoncalves <brendom.vi@gmail.com>
2024-05-28 22:45:55 -0300

    Fix: corrigindo responses da task

--- src/mytodo.shareable/Responses/Task/GetTaskResponse.cs
+++ src/mytodo.shareable/Responses/Task/GetTaskResponse.cs
@@ -3,7 +3,7 @@ namespace mytodo.shareable.Responses.Task;
 public record GetTaskResponse(
     int TaskId,
     string Title,
-    string Description,
+    string? Description,
     DateTime DueDate,
     string Status,
     string Priority,
--- src/mytodo.shareable/Responses/Task/UpdateTaskResponse.cs
+++ src/mytodo.shareable/Responses/Task/UpdateTaskResponse.cs
@@ -2,10 +2,5 @@ namespace mytodo.shareable.Responses.Task;
 
 public record UpdateTaskResponse(
     int TaskId,
-    string Title,
-    string Description,
-    DateTime DueDate,
-    string Status,
-    string Priority,
-    int UserId    
+    string Title  
 );
