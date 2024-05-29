cfa656cdf31f4f7a276210e504ba2aa8c5de43ad
BrendomGoncalves <brendom.vi@gmail.com>
2024-05-28 22:57:09 -0300

    Feat: Implementando controller de task e user

--- src/mytodo.api/Controllers/TaskController.cs
+++ src/mytodo.api/Controllers/TaskController.cs
@@ -1,6 +1,50 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using mytodo.shareable.Excecoes;
+using mytodo.shareable.Requests.Task;
+using mytodo.shareable.Responses.Task;
+
 namespace mytodo.api.Controllers;
 
-public class TaskController
+[ApiController]
+[Route("[controller]")]
+public class TaskController : BaseController
 {
-    // TODO: Implementar controller das tasks
+    public TaskController(IMediator mediator) : base(mediator) {}
+
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateTaskResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CreateTaskResponse>> CreateTaskAsync([FromBody] CreateTaskRequest request) =>
+        await SendCommand(request);
+    
+    [HttpDelete]
+    [ProducesResponseType(typeof(DeleteTaskResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<DeleteTaskResponse>> DeleteTaskAsync([FromBody] DeleteTaskRequest request) =>
+        await SendCommand(request);
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(GetTaskResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<GetTaskResponse>> GetTaskAsync(int id)
+    {
+        var request = new GetTaskRequest{TaskId = id};
+        return await SendCommand(request);
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(typeof(List<GetTaskResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<List<GetTaskResponse>>> GetTasksAsync()
+    {
+        var request = new GetAllTaskRequest();
+        return await SendCommand(request);
+    }
+    
+    [HttpPut]
+    [ProducesResponseType(typeof(UpdateTaskResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<UpdateTaskResponse>> UpdateTaskAsync([FromBody] UpdateTaskRequest request) =>
+        await SendCommand(request);
 }
--- src/mytodo.api/Controllers/UserController.cs
+++ src/mytodo.api/Controllers/UserController.cs
@@ -1,6 +1,50 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using mytodo.shareable.Excecoes;
+using mytodo.shareable.Requests.User;
+using mytodo.shareable.Responses.User;
+
 namespace mytodo.api.Controllers;
 
-public class UserController
+[ApiController]
+[Route("[controller]")]
+public class UserController : BaseController
 {
-    // TODO: Implementar controller do usuário
+    public UserController(IMediator mediator) : base(mediator) {}
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateUserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CreateUserResponse>> CreateUserAsync([FromBody] CreateUserRequest request) =>
+        await SendCommand(request);
+    
+    [HttpDelete]
+    [ProducesResponseType(typeof(DeleteUserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<DeleteUserResponse>> DeleteUserAsync([FromBody] DeleteUserRequest request) =>
+        await SendCommand(request);
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(GetUserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<GetUserResponse>> GetUserAsync(int id)
+    {
+        var request = new GetUserRequest { UserId = id };
+        return await SendCommand(request);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<GetUserResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<List<GetUserResponse>>> GetUsersAsync()
+    {
+        var request = new GetAllUserRequest();
+        return await SendCommand(request);
+    }
+    
+    [HttpPut]
+    [ProducesResponseType(typeof(UpdateUserResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResultadoErro), StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<UpdateUserResponse>> UpdateUserAsync([FromBody] UpdateUserRequest request) =>
+        await SendCommand(request);
 }
