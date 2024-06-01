80db51c8922385ad7d3ed29dd4f9d9e74fbbb62a
BrendomGoncalves <brendom.vi@gmail.com>
2024-06-01 01:26:09 -0300

    Fix: Modificando a busca para ter retorno nulo

--- src/mytodo.domain/Repository/ITaskRepository.cs
+++ src/mytodo.domain/Repository/ITaskRepository.cs
@@ -5,7 +5,7 @@ namespace mytodo.domain.Repository;
 public interface ITaskRepository
 {
     Task<TaskEntity> CreateTaskAsync(TaskEntity task);
-    Task<TaskEntity> GetTaskByIdAsync(int id);
+    Task<TaskEntity?> GetTaskByIdAsync(int id);
     Task<List<TaskEntity>> GetTasksAsync();
     Task<TaskEntity> DeleteTaskAsync(TaskEntity task);
     Task<TaskEntity> UpdateTaskAsync(TaskEntity task);
--- src/mytodo.domain/Repository/IUserRepository.cs
+++ src/mytodo.domain/Repository/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace mytodo.domain.Repository;
 public interface IUserRepository
 {
     Task<UserEntity> CreateUserAsync(UserEntity user);
-    Task<UserEntity> GetUserByIdAsync(int id);
+    Task<UserEntity?> GetUserByIdAsync(int id);
     Task<List<UserEntity>> GetUsersAsync();
     Task<UserEntity> UpdateUserAsync(UserEntity user);
     Task<UserEntity> DeleteUserAsync(UserEntity user);
