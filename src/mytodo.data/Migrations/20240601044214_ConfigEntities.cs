2682be06a8ff25c1f9cb732dd41edf9539075610
BrendomGoncalves <brendom.vi@gmail.com>
2024-06-01 01:49:32 -0300

    Feat: Atualizando banco, criando migration

---
+++ src/mytodo.data/Migrations/20240601044214_ConfigEntities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mytodo.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Task");
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DataVencimento",
+                table: "Task",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataVencimento",
+                table: "Task");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Task",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
